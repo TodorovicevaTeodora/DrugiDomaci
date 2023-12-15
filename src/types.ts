e8a72d715689259daa9db0d10cf3afef33d6df21
TodorovicevaTeodora <todorovic.teaa@gmail.com>
2023-12-15 23:52:05 +0100

    Dodati tipovi podataka i biblioteke

---
+++ src/types.ts
@@ -0,0 +1,74 @@
+export interface Item {
+    id: number,
+    createdAt: string,
+    name: string,
+    price: number,
+    disabled: boolean,
+    storeId: number
+}
+export type OrderStatus = 'pending' | 'accepted' | 'rejected' | 'prepared'
+export type DriverStatus = 'assigned' | 'picked_up' | 'delivered'
+export interface BaseOrder {
+    id: number,
+    createdAt: string,
+    address: string,
+    status: OrderStatus,
+    driverStatus: DriverStatus,
+    lat: number,
+    lng?: number,
+    rating?: number,
+    clientId: number,
+    driverId: number,
+    storeId: number,
+    prepTime: number,
+    deliveryTime: number,
+}
+export interface Order {
+    id: number,
+    createdAt: string,
+    address: string,
+    status: OrderStatus,
+    driverStatus: DriverStatus,
+    lat: number,
+    lng: number,
+    rating?: number,
+    clientId: number,
+    driverId: number,
+    storeId: number,
+    items: OrderItem[],
+    prepTime: number,
+    deliveryTime: number,
+}
+export interface Pagination<T> {
+    data: T[],
+    page: number,
+    total: number
+}
+
+export interface OrderItem {
+    id: number,
+    createdAt: string,
+    item: Item,
+    count: number,
+    price: number
+}
+
+export interface Store {
+    id: number,
+    createdAt: string,
+    lat: number,
+    lng: number,
+    address: string,
+    name: string,
+    items: Item[]
+}
+export type UserType = 'client' | 'driver' | 'merchant' | 'admin'
+export interface User {
+    id: number,
+    createdAt: string,
+    name: string,
+    email: string,
+    phone: string,
+    userType: UserType
+
+}
